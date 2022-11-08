675d9e33abf8e8df8d11aef130bf43deac223283
AntonKilk <ant.kilk@gmail.com>
2022-11-08 10:30:45 +0200

    error message added

--- corewar/src/init.c
+++ corewar/src/init.c
@@ -6,7 +6,7 @@
 /*   By: akilk <akilk@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:38:58 by akilk             #+#    #+#             */
-/*   Updated: 2022/11/08 07:15:23 by akilk            ###   ########.fr       */
+/*   Updated: 2022/11/08 10:27:34 by akilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,16 @@ void	init_champion(t_champion *champion)
 
 	new_champion = (t_champion *)ft_memalloc(sizeof (t_champion));
 	if (!new_champion)
-		exit (1); //add error msg?
+		error(NULL, "Allocation failed in init_champion()");
 }
 
-void	init_vm(t_vm *vm)
+t_vm	*init_vm(void)
 {
+	t_vm	*vm;
+
 	vm = (t_vm *)ft_memalloc(sizeof (t_vm));
 	if (!vm)
-		exit (1); //add error msg?
+		error(NULL, "Allocation failed in init_vm()");
 	vm->champions_num = 0;
+	return (vm);
 }
