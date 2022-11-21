34f93bf49e58d4efabc8bfdd99a54dabaed11736
Otto Koponen <ottokoponen01@gmail.com>
2022-11-21 17:19:39 +0200

    rewrote generator.c, deleted argument_codes.c. Needs debugging, but values are now converted from array to int form

--- asm/src/generator.c
+++ asm/src/generator.c
@@ -12,102 +12,119 @@
 
 #include "../includes/asm.h"
 
-/*
-Take all finalized individual hex codes and join them all into the final
-hexcode string consisting of statement code, argument type code and 
-the individual codes for all arguments. NULL values are skipped
-*/
-void	make_final_hex(t_input *data)
+void	save_atc(t_input *data)
 {
-
-}
-
-/*
-Function to convert the binary version of the argument type code into
-hexadecimal including 0x prefix.
-*/
-char	*conv_binary_to_hex(char *binary)
-{
-
-}
-
-/*
-Different argument types have defined binary codes that correspond to their
-types. Here we join those codes together for every argument to create the
-binary version of this argument type code.
-Arguments of type T_DIR have the code 10
-Arguments of type T_DIR have the code 01
-Arguments of type T_DIR have the code 11
-For no argument type we must still add code 00.
-*/
-char	*find_arg_type(int type, char *temp)
-{
-	if (!type)
-		return (ft_strcat(temp, "00"));
-	if (type = 1)
-		return (ft_strcat(temp, "10"));
-	if (type = 2)
-		return (ft_strcat(temp, "01"));
-	if (type = 3)
-		return (ft_strcat(temp, "11"));
-}
-
-/*
-Find the argument type for given instruction by combinging all values of the
-argument types in a large string and then converting the final result into
-hexadecimal
-1 = T_DIR = 10, 2 = T_REG  = 01, 3 = T_IND 11
-*/
-void	find_atc(t_input *data)
-{
-	char	*temp;
 	int		i;
+	char	*temp;
 	int		j;
-
-	i = 1;
+	
+	i = 0;
 	j = 0;
-	temp = malloc(sizeof(char) * 9);
+	temp = (char *)malloc(sizeof(char) * 9);
 	if (!temp)
-		error(1, "malloc error in find_atc");
+		exit(1);
 	temp[8] = '\0';
-
-	while (data->arg_type[i])
-		temp = find_arg_type(data->arg_type[i++], temp);
-	temp = find_arg_type(0, temp);
-	data->argument_type_code = conv_binary_to_hex(temp);
-	free (temp);
+	while (j < 9)
+	{
+		if (data->arg_type[i] == 1)
+		{
+			temp[j++] = '0';
+			temp[j++] = '1';
+		}
+		else if (data->arg_type[i] == 2)
+		{
+			temp[j++] = '1';
+			temp[j++] = '0';
+		}
+		else if (data->arg_type[i] == 3)
+		{
+			temp[j++] = '1';
+			temp[j++] = '1';
+		}
+		else if (data->arg_type[i] == 0)
+		{
+			temp[j++] = '0';
+			temp[j++] = '0';
+		}
+		i++;
+	}
+//	data->argument_type_code = ft_btoi(temp);
+	free(temp);
 }
 
-/*
-For the statement code we only need to convert statement code to
-hexadecimal for final hexcode for this statement.
-*/
-void	find_statement_code(t_input *data)
-{
-	data->statement_code = ft_itoa_base(data->statement, 16);
-	//make sure statement code is in format 0x00
-}
-
-/*
-Function to generate all different hex codes and fill them in the struct.
-*/
-void	generate_code(t_input *data, int current_pos)
-{
-	find_statement_code(data);
-	find_atc(data);
-	find_argument_codes(data, current_pos);
-	make_final_hex(data);
-}
-
-/*
-Initial function to parse through all statements and then join them
-together to generate the final hexcode (opcode or bytecode) for all
-statements in the array and keep track of which byte we are on currently.
-*/
-void	generator(t_input **data)
+int	find_number(char *current_arg)
 {
 	int	i;
-	int	current_pos;
+
+	i = 0;
+	if (current_arg[0] == '%' || current_arg[0] == 'r')
+		i++;
+	return (ft_atoi(&current_arg[i]));
+}
+
+int	is_label_call(char *current_arg)
+{
+	int	i;
+
+	i = 0;
+	while (current_arg[i])
+	{
+		if (current_arg[i] == ':')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	find_label_addr(t_input **data, char *current_arg, int curr_arg, int curr_struct)
+{
+	int	i;
+
+	i = 0;
+	while (data[i])
+	{
+		if (data[i]->is_label)
+		{
+			if (ft_strcmp(data[i]->label_name, current_arg))
+			{
+				data[curr_struct]->arg_values[curr_arg] = data[i]->label_addr - data[curr_struct]->current_bytes;
+				return ;
+			}
+		}
+		i++;
+	}
+}
+
+void	save_argument_values(t_input **original_data, t_input *data, int current_arg, int curr_struct)
+{
+	int	num;
+
+	num = 0;
+	if (is_label_call(data->args[current_arg]))
+		find_label_addr(original_data, data->args[current_arg], current_arg, curr_struct);
+	else
+		data->arg_values[current_arg] = find_number(data->args[current_arg]);
+}
+
+void	generate_input(t_input **original_data, t_input *data, int curr_struct)
+{
+	int	i;
+
+	i = 0;
+	save_atc(data);
+	while (data->args[i])
+	{
+		save_argument_values(original_data, data, i, curr_struct);
+		i++;
+	}
+}
+
+
+
+void	generator(t_input **data)
+{
+	int			i;
+	int			current_pos;
 
 	i = 0;
 	current_pos = 0;
@@ -115,8 +132,32 @@ void	generator(t_input **data)
 	{
 		data[i]->current_bytes = current_pos;
 		if (!data[i]->is_label)
-			generate_input(data[i], current_pos);
-		i++;
+			generate_input(data, data[i], i);
 		current_pos += data[i]->byte_size;
+		i++;
 	}
+}
+
+int main(void)
+{
+	t_input *data[1];
+
+//st r1, r2
+	data[0]->statement = 3;
+	data[0]->is_label = 0;
+	data[0]->label_addr = 0;
+	data[0]->label_name[0] = '\0';
+	data[0]->byte_size = 4;
+	data[0]->arg_size[0] = 1;
+	data[0]->arg_size[1] = 1;
+	data[0]->arg_size[2] = 0;
+	data[0]->arg_type[0] = 1;
+	data[0]->arg_type[1] = 1;
+	data[0]->arg_type[2] = 0;
+	data[0]->args[0][0] = 'r';
+	data[0]->args[0][1] = '1';
+	data[0]->args[1][0] = 'r';
+	data[0]->args[1][1] = '2';
+	generator(data);
+	return (0);
 }
