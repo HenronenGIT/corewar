6b7dba93b263b7b2efdd475d2078e726aee68a06
wsdonnelly <wsdonnelly@gmail.com>
2022-12-16 13:29:45 +0200

    updated tester.sh

--- corewar/tester.sh
+++ corewar/tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#for CHAMP in tests/*.cor; do ./corewar $CHAMP $CHAMP $CHAMP $CHAMP -v 1 >> mytest.txt; done
-#for CHAMP in tests/*.cor; do ../eval_tests/corewar $CHAMP $CHAMP $CHAMP $CHAMP  >> origtest.txt; done
+# output will be diff'd but I reccomend using VScode's 'compare selected feature as well'
+# the competitions can often take a long time (minutes)
 
 champ1=$(ls cor_samples | sort -R | tail -n 1);
 champ2=$(ls cor_samples | sort -R | tail -n 1);
@@ -16,4 +16,5 @@ echo $champ4
 ./corewar cor_samples/$champ1 cor_samples/$champ2 cor_samples/$champ3 cor_samples/$champ4 -v 1 > mytest.txt &
 ../eval_tests/corewar cor_samples/$champ1 cor_samples/$champ2 cor_samples/$champ3 cor_samples/$champ4  > origtest.txt &
 wait
-#diff mytest.txt origtest.txt
+
+diff mytest.txt origtest.txt
