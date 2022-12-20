a93643e87e2a991d427ef43adf3a0da7f9f0c0a3
Henri <henrimaronen@gmail.com>
2022-12-20 10:15:04 +0200

    comment removed

--- assembler/src/syntax_analyzer.c
+++ assembler/src/syntax_analyzer.c
@@ -12,19 +12,6 @@
 
 #include "../includes/asm.h"
 
-// int lookup(const char *string)
-// {
-// 	size_t i;
-
-// 	i = 0;
-// 	while (g_op_tab[i].instruction != NULL)
-// 	{
-// 		if (ft_strcmp(string, g_op_tab[i].instruction) == 0)
-// 			return (g_op_tab[i].op_code);
-// 		i += 1;
-// 	}
-// 	return (-1);
-// }
 int lookup(const char *string)
 {
 	size_t i;
