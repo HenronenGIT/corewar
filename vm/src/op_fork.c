216f707218cfedb431624604a54589f56a167420
William Donnelly <wdonnell@c2r5p3.hive.fi>
2023-01-11 15:29:32 +0200

    norminette fixed

--- vm/src/op_fork.c
+++ vm/src/op_fork.c
@@ -6,7 +6,7 @@
 /*   By: wdonnell <wdonnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:27:43 by wdonnell          #+#    #+#             */
-/*   Updated: 2023/01/11 15:16:01 by wdonnell         ###   ########.fr       */
+/*   Updated: 2023/01/11 15:26:36 by wdonnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ void	op_fork(t_process *process, t_data *data)
 	data->parent = process;
 	add_process(data, &data->head, ++data->new_process_id);
 	if (data->verbosity & 0x04)
-		ft_printf("P%5d | fork %d (%d)\n", process->id, idx, process->cursor + idx % IDX_MOD);
+		ft_printf("P%5d | fork %d (%d)\n", process->id, idx, \
+		process->cursor + idx % IDX_MOD);
 	print_byte_jumps(process, data);
 }
 
